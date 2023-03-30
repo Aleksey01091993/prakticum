b05d4511cee60e5a2debb11e1363adef351832b7
Aleksey01091993 <128576817+Aleksey01091993@users.noreply.github.com>
2023-03-30 19:00:39 +0300

    mission complite

--- praktikum/src/Praktikum.java
+++ praktikum/src/Praktikum.java
@@ -17,7 +17,6 @@ public class Praktikum {
 
     public static boolean isLeapYear(int year) {
         // здесь нужно определить, является ли переданный год високосным
-        if (year % 4 == 0)return true;
-        return false;
+        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
     }
 }
